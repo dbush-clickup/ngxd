45446acd4792f779916d3e830cdeddee2632bd23
Dave Bush <dbush@clickup.com>
2024-01-29 19:31:53 -0500

    all test passing

--- projects/core/src/testing/test-case/reports/simple-lifecycle.report.ts
+++ projects/core/src/testing/test-case/reports/simple-lifecycle.report.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { hasProperty } from '../../../lib/property-def';
 import { ContextState, LifecycleReport, LifecycleState } from '../test-case.interfaces';
 
@@ -7,9 +8,19 @@ export class SimpleLifecycleReport extends LifecycleReport {
   }
 
   canReport<TComponent>(oldState: ContextState, newState: ContextState): boolean {
+    if(!newState.type || !hasProperty(newState.type.prototype, this.name)) {
+      return false;
+    }
     const hasChanged = oldState.dynamic.name !== newState.dynamic.name ||
       oldState.dynamic.label !== newState.dynamic.label;
-    return hasChanged && newState.type && hasProperty(newState.type.prototype, this.name);
+    // Angular deals with onPush components differently, so we need to check for that
+    // - When onPush is set, the value never calls setter or otherwise makes changes
+    //   if the Input doesn't change.
+    const onPush = newState.type && (newState.type as unknown as {ɵcmp: {onPush: boolean}}).ɵcmp.onPush;
+    if(onPush && !hasChanged) {
+      return false;
+    }
+    return true;
   }
 
   report<TComponent>(oldState: ContextState, newState: ContextState): LifecycleState {
